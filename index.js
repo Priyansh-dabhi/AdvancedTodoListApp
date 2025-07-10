d2acaedb9810f10e7a7a7226d11e0bdab6d849a6
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-07-10 22:34:10 +0530

    feat: gesture-handler added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-
+import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './Src/App.tsx';
 import { name as appName } from './app.json';
