dd31930a158d01740c6a649bd1098d8d3c805e0b
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-07-27 19:09:02 +0530

    feat: configured tailwind.config.ts with appropriate content path!

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Src/App.{js,jsx,ts,tsx}",
+    "./Src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
