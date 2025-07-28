75d5cedceec196bb488e63c501c86e452654c9b6
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-07-28 21:54:49 +0530

    feat: nativewind removed!

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,43 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { withNativeWind } = require('nativewind/metro');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('@react-native/metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+// module.exports = withNativeWind(config, { input: './global.css' })
+
+
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { withNativeWind } = require('nativewind/metro');
+
+// // Get the base config
+// const defaultConfig = getDefaultConfig(__dirname);
+
+// // Merge NativeWind with Metro
+// const config = withNativeWind(defaultConfig, {
+//   input: './global.css', 
+// });
+
+// module.exports = config;
+
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { withNativeWind } = require('nativewind/metro');
+
+// const config = {};
+
+// module.exports = withNativeWind(
+//     mergeConfig(getDefaultConfig(__dirname), config)
+// );
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-module.exports = withNativeWind(config, { input: './global.css' })
