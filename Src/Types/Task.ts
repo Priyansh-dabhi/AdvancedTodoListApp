bb0a54b67102baeff277b1d1923e9e3cc06b8255
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-03 21:26:04 +0530

    feat: some parameter removed

--- Src/Types/Task.ts
+++ Src/Types/Task.ts
@@ -1,13 +1,13 @@
     // TaskInput:
 export type Task = {
     id: string;  
-    task: string;  
-    timestamp:string,        // or string if you use UUID
+    // task: string;  
+    // timestamp:string,        // or string if you use UUID
     title: string;
     category?: [string];
     completed?: boolean;
     date?: string;      // optional, if you want to track when the task was created
     
-    success?: (res: any) => void;
-    error?: (err: any) => void;
+    // success?: (res: any) => void;
+    // error?: (err: any) => void;
 };
