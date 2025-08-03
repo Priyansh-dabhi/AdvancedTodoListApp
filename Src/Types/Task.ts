d5d83157ef78e3819141869bfa496b196ecbdbc7
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-03 14:05:00 +0530

    feat: new parameter added

--- Src/Types/Task.ts
+++ Src/Types/Task.ts
@@ -1,8 +1,13 @@
     // TaskInput:
 export type Task = {
-    id: string;          // or string if you use UUID
+    id: string;  
+    task: string;  
+    timestamp:string,        // or string if you use UUID
     title: string;
     category?: [string];
     completed?: boolean;
     date?: string;      // optional, if you want to track when the task was created
+    
+    success?: (res: any) => void;
+    error?: (err: any) => void;
 };
