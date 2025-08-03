c064082e5c8700c51e436b5a4d752de371b6470b
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-03 14:01:59 +0530

    feat(DB): database and table task created and insert method added

--- Src/DB/Database.ts
+++ Src/DB/Database.ts
@@ -20,3 +20,40 @@
 // };
 
 // export { getDBConnection, createTables };
+
+import SQLite from 'react-native-sqlite-storage'
+import { Task } from '../Types/Task';
+
+const db = SQLite.openDatabase({
+    name: 'todolist.db', location: 'default'
+}
+,()=> { // call backfor success
+    console.log('Database created successfully');
+}, (error)=>{ // call back for error
+    console.log('error'+error);
+});
+
+// table creation
+
+export const initDB = () => {
+    db.transaction(tx=> {
+        tx.executeSql( // HERe below first we create the table then in () we define the columns
+            `CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            task TEXT NOT NULL,
+            timestamp TEXT NOT NULL,
+            );`)
+    })
+}
+
+// Crud
+
+
+export const insertTask = ({task,timestamp,success,error}:Task) => {
+    db.transaction(tx=>{
+        tx.executeSql(
+            `INSERT INTO tasks (task, timestamp) VALUES (?, ?)`,[task, timestamp],(_,response) => success?.(response),
+            (_,err)=> error?.(err)
+        )
+    })
+}
