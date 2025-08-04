2ae37be7d95d6ae527aa7fd8af9f05f8690c4d73
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-04 16:08:24 +0530

    feat: props updated

--- Src/Types/Task.ts
+++ Src/Types/Task.ts
@@ -1,13 +1,12 @@
     // TaskInput:
 export type Task = {
     id: string;  
-    // task: string;  
-    // timestamp:string,        // or string if you use UUID
-    title: string;
+    task: string;  
+    timestamp:string,        // or string if you use UUID
     category?: [string];
     completed?: boolean;
     date?: string;      // optional, if you want to track when the task was created
     
-    // success?: (res: any) => void;
-    // error?: (err: any) => void;
+    success?: (res: any) => void;
+    error?: (err: any) => void;
 };
