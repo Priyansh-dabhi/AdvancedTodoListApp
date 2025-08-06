133191cc1dffacf3bebb48336555185f03c11745
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-06 22:14:36 +0530

    temparary

--- Src/Types/TaskForDB.ts
+++ Src/Types/TaskForDB.ts
@@ -1,8 +1,12 @@
     // TaskInput:
 export type TaskForDB = {
-    id: string;  
+    id: number;  
     task: string;  
     timestamp:string,        // or string if you use UUID
+    category?: [string];
+    completed?: boolean;
+    date?: string;      // optional, if you want to track when the task was created
+    
     success?: (res: any) => void;
     error?: (err: any) => void;
 };
