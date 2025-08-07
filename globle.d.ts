4a28d0ec91335896c151c5d114152f41536a653f
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-07 23:39:31 +0530

    feat: declaration of appwrite info

---
+++ globle.d.ts
@@ -0,0 +1,7 @@
+// global.d.ts
+declare module '@env' {
+    export const APPWRITE_ENDPOINT: string;
+    export const APPWRITE_PROJECT_ID: string;
+    export const APPWRITE_DATABASE_ID: string;
+    export const APPWRITE_COLLECTION_ID: string;
+}
