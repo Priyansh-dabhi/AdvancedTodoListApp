f8b86dc8b0ea5e4b807afcd627bb572ec5f09991
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-07 23:09:40 +0530

    feat: add react-native-dotenv configuration

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@': './', // NOT './src'
-        },
-      },
-    ],
+            [
+              'module-resolver',
+              {
+                root: ['./'],
+                alias: {
+                  '@': './', // NOT './src'
+                },
+              },
+            ],
+            ['react-native-dotenv', {
+                moduleName: '@env',
+                path: '.env',
+                blacklist: null,
+                whitelist: null,
+                safe: false,
+                allowUndefined: true,
+                verbose: false,
+            }],
     // 'nativewind/babel',
     'react-native-reanimated/plugin',
   ],
