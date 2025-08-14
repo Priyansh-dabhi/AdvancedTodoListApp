d33c8c68337b2b2e090ace5fd8cb71356816a04f
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-14 19:04:27 +0530

    feat: add back icon

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,8 +1,10 @@
 import target from "../assets/icons/target.png";
 import target1 from "../assets/icons/target1.png";
 import target2 from "../assets/icons/target2.png";
+import back from "../assets/icons/back.png";
 export default {
   target,
   target1,
-  target2
+  target2,
+  back,
 };
