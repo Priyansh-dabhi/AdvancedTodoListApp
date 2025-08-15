8ef69275318de64a0aa49caedb58eeb0771925d5
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-15 14:05:52 +0530

    feat: add due date and time props

--- Src/Types/Task.ts
+++ Src/Types/Task.ts
@@ -5,8 +5,9 @@ export type Task = {
     task: string;  
     timestamp:string,        // or string if you use UUID
     // category?: [string];
+    DueDate: string;
+    DueTime: string;
     completed?: boolean;
-    // date?: string;      // optional, if you want to track when the task was created
     isSynced?: boolean; // used to check if the task is synced with the appwrite or not
     success?: (res: any) => void;
     error?: (err: any) => void;
