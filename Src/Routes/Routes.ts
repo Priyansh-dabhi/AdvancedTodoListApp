d4807d45540dd9b99e790078283a9eb172eb2603
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-15 22:29:29 +0530

    feat: change prop type

--- Src/Routes/Routes.ts
+++ Src/Routes/Routes.ts
@@ -11,7 +11,7 @@ export type AppStackParamList = {
     Temperary:undefined
     EditProfile:undefined
     Map:undefined
-    EditTask:undefined
+    EditTask:{taskId: number; task?: string }
 }
 
 const Routes =  {
