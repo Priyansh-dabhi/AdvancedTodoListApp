35c6ea66e57d30d7b8aa5e7afa3ab4466c17f560
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-17 23:45:19 +0530

    feat: ui changed

--- Src/Screens/Home.tsx
+++ Src/Screens/Home.tsx
@@ -1,6 +1,15 @@
-import { StyleSheet, Text, View, Pressable, TouchableOpacity, FlatList, Button, Alert } from 'react-native'
-import BouncyCheckbox from 'react-native-bouncy-checkbox'
-import React ,{useContext, useEffect, useState} from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+  FlatList,
+  Button,
+  Alert,
+} from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../Context/AppwriteContext';
 import { getCurrentUser, logout } from '../Service/Service';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
@@ -20,171 +29,184 @@ const Home = () => {
     const navigation = useNavigation<any>();
     //Greeting the user
     const [greeting, setGreeting] = useState('');
-    
-        const [tasks, setTasks] = useState<Task[]>([]);
-        const [modalVisible, setModalVisible] = useState(false);
-        const [searchQuery, setSearchQuery] = useState('');
 
-        // clear serch query
-        const clearText = () => {
-            setSearchQuery('');
-        }; 
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
 
-        // Greeting the user
-        useEffect(()=> {
+    // clear serch query
+    const clearText = () => {
+        setSearchQuery('');
+    };
+
+    // Greeting the user
+    useEffect(() => {
         const date = new Date();
-        const hours = date.getHours(); 
+        const hours = date.getHours();
         if (hours < 12) {
-            setGreeting('Good Morning');
+        setGreeting('Good Morning');
         } else if (hours < 17) {
-            setGreeting('Good Afternoon');
+        setGreeting('Good Afternoon');
         } else {
-            setGreeting('Good Evening');
+        setGreeting('Good Evening');
         }
-    },[])
+    }, []);
 
     // Fetch tasks from DB
     const fetchTasksFromDB = () => {
-    getAllTasks((fetchedTasks: Task[]) => {
+        getAllTasks((fetchedTasks: Task[]) => {
         // Map over the tasks and set completed to false for all of them
         const uncheckedTasks = fetchedTasks.map(task => ({
             ...task,
             completed: false,
         }));
         setTasks(uncheckedTasks);
-        console.log("Fetched and Unchecked Tasks:", uncheckedTasks);
-    });
-};
-// fetchTasksFromDB
+        console.log('Fetched and Unchecked Tasks:', uncheckedTasks);
+        });
+    };
+    // fetchTasksFromDB
     useEffect(() => {
         fetchTasksFromDB();
     }, []);
 
-const handleCreateTask = () => {
-    fetchTasksFromDB();
-};
-const handleTaskDeleteOnCheckboxPress = (taskId: number) => {
-    deleteTask(taskId,
+    const handleCreateTask = () => {
+        fetchTasksFromDB();
+    };
+    const handleTaskDeleteOnCheckboxPress = (taskId: number) => {
+        deleteTask(
+        taskId,
         () => {
             console.log('Deleted successfully');
             // Call the new function to refresh the list and uncheck all items
             fetchTasksFromDB();
         },
-        (err) => {
+        err => {
             Alert.alert('Error', 'Failed to delete task');
-        }
-    );
-};
-const handleTaskCompletionToggle = (taskId: number, currentStatus: boolean) => {
-    // The completed parameter should be a boolean, not a number.
-    const newStatus = !currentStatus;
+        },
+        );
+    };
+    const handleTaskCompletionToggle = (
+        taskId: number,
+        currentStatus: boolean,
+    ) => {
+        // The completed parameter should be a boolean, not a number.
+        const newStatus = !currentStatus;
 
-    updateTaskCompletion({
+        updateTaskCompletion({
         id: taskId,
         completed: newStatus, // Pass the boolean directly
         success: () => {
             console.log(`Task ${taskId} completion toggled to ${newStatus}`);
             fetchTasksFromDB(); // Re-fetch all tasks to update the UI
         },
-        error: (err) => {
+        error: err => {
             console.error('Failed to update task completion:', err);
             Alert.alert('Error', 'Could not update task status.');
-        }
-    });
-};
+        },
+        });
+    };
     return (
-
         <View style={styles.container}>
-            {/* <Text style={styles.title}>Welcome <Text style={{color:'#f02e65'}}>{username}</Text> to Home Screen 🎉 </Text>
+        {/* <Text style={styles.title}>Welcome <Text style={{color:'#f02e65'}}>{username}</Text> to Home Screen 🎉 </Text>
 
-            <Pressable style={styles.logoutBtn} onPress={handleLogout}>
-                <Text style={styles.logoutText}>Logout</Text>
-            </Pressable>                     */}
+                <Pressable style={styles.logoutBtn} onPress={handleLogout}>
+                    <Text style={styles.logoutText}>Logout</Text>
+                </Pressable>                     */}
 
-            {/* List of tasks */}
-                {/* <ScrollView horizontal={true} style={{marginTop:20,marginBottom:20 ,borderWidth:1, }}>
-                    <TouchableOpacity style={{backgroundColor:'red'}}><Text>Add</Text></TouchableOpacity>
-                    
-                </ScrollView> */}
-                <View style={styles.headerContainer}>
-                    <View style={styles.headerLeft}>
-                        <Text style={styles.welcomeText}>{greeting} 👋</Text>
-                        <Text style={styles.usernameText}>Let's manage your tasks!</Text>
-                    </View>
+        {/* List of tasks */}
+        {/* <ScrollView horizontal={true} style={{marginTop:20,marginBottom:20 ,borderWidth:1, }}>
+                        <TouchableOpacity style={{backgroundColor:'red'}}><Text>Add</Text></TouchableOpacity>
+                        
+                    </ScrollView> */}
+        <View style={styles.headerContainer}>
+            <View style={styles.headerLeft}>
+            <Text style={styles.welcomeText}>{greeting} 👋</Text>
+            <Text style={styles.usernameText}>Let's manage your tasks!</Text>
+            </View>
 
-    
-                    <View style={styles.searchRow}>
-                        <View style={styles.searchContainer}>
-                            <TextInput
-                                placeholder="Search tasks..."
-                                clearButtonMode='always'
-                                placeholderTextColor="#888"
-                                style={styles.searchInput}
-                                value={searchQuery}
-                                onChangeText={setSearchQuery}
-                            />
-                            {searchQuery.length > 0 && (
-                                <TouchableOpacity onPress={clearText} style={styles.clearButton}>
-                                        <Icon name="close-circle" size={20} color="#888" />
-                                </TouchableOpacity>
-                            )}
-                        </View>
-                            <TouchableOpacity style={styles.searchButton}>
-                                <Icon name="search" size={30} color="#FF6B6B" style={{ marginRight: 8 ,marginLeft:8}} />
-                            </TouchableOpacity>
-                        </View>
-                    </View>   
-
-                <FlatList
-                    data={tasks.filter(task =>  task.task?.toLowerCase().includes(searchQuery.toLowerCase()))}
-                    keyExtractor={(item) => item.id.toString()}
-                    // style={{ width: '100%',borderWidth:1 }}  
-                    renderItem={({ item }) => (
-                    <TouchableOpacity 
-                    delayPressIn={100}
-                    delayPressOut={100}
-                    delayLongPress={500}
-                    hitSlop={{ top: 10, bottom: 10, left: 20, right: 20 }}
-                    onPress={()=> {navigation.navigate(Routes.EditTask)}}
-                    >
-                        <View style={styles.taskCard}>
-                            <View style={{alignItems:'center',justifyContent:'center'}}>
-                                <BouncyCheckbox
-                                fillColor='#FF6B6B'
-                                isChecked={item.completed}
-                                onPress={()=>handleTaskDeleteOnCheckboxPress(item.id)}
-                                />
-                            </View>
-                            <View style={{flex:1}}>
-                                <Text style={styles.taskItem}>{item.task}</Text>
-                                <Text style={styles.taskItem}>{item.timestamp}</Text> 
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                    )
-                }   
-                    contentContainerStyle={{ padding: 10,marginLeft:15,marginRight:15,marginBottom:20 }}
-                    style={styles.flatList}
+            <View style={styles.searchRow}>
+            <View style={styles.searchContainer}>
+                <TextInput
+                placeholder="Search tasks..."
+                clearButtonMode="always"
+                placeholderTextColor="#888"
+                style={styles.searchInput}
+                value={searchQuery}
+                onChangeText={setSearchQuery}
                 />
-                {/* Floating "+" button */}
-                <TouchableOpacity
-                    style={styles.fab}
-                    onPress={() => setModalVisible(true)}
-                >
-                    <Icon name="add" size={32} color="white" />
-
+                {searchQuery.length > 0 && (
+                <TouchableOpacity onPress={clearText} style={styles.clearButton}>
+                    <Icon name="close-circle" size={20} color="#888" />
                 </TouchableOpacity>
-                {/* Bottom Modal */}
-            <AddTaskModal
-                isVisible={modalVisible}
-                onClose={() => setModalVisible(false)}
-                onCreate={handleCreateTask}
-            />
-            
+                )}
+            </View>
+            <TouchableOpacity style={styles.searchButton}>
+                <Icon
+                name="search"
+                size={30}
+                color="#FF6B6B"
+                style={{ marginRight: 8, marginLeft: 8 }}
+                />
+            </TouchableOpacity>
+            </View>
         </View>
-    )
-}
-export default Home
+
+        <FlatList
+            data={tasks.filter(task =>
+            task.task?.toLowerCase().includes(searchQuery.toLowerCase()),
+            )}
+            keyExtractor={item => item.id.toString()}
+            // style={{ width: '100%',borderWidth:1 }}
+            renderItem={({ item }) => (
+            <TouchableOpacity
+                delayPressIn={100}
+                delayPressOut={100}
+                delayLongPress={500}
+                hitSlop={{ top: 10, bottom: 10, left: 20, right: 20 }}
+                onPress={() => {
+                navigation.navigate(Routes.EditTask);
+                }}
+            >
+                <View style={styles.taskCard}>
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <BouncyCheckbox
+                    fillColor="#FF6B6B"
+                    isChecked={item.completed}
+                    onPress={() => handleTaskDeleteOnCheckboxPress(item.id)}
+                    />
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.taskItem}>{item.task}</Text>
+                    <Text style={styles.taskItem}>{item.timestamp}</Text>
+                </View>
+                </View>
+            </TouchableOpacity>
+            )}
+            contentContainerStyle={{
+            padding: 10,
+            marginLeft: 15,
+            marginRight: 15,
+            marginBottom: 20,
+            }}
+            style={styles.flatList}
+        />
+        {/* Floating "+" button */}
+        <TouchableOpacity
+            style={styles.fab}
+            onPress={() => setModalVisible(true)}
+        >
+            <Icon name="add" size={32} color="white" />
+        </TouchableOpacity>
+        {/* Bottom Modal */}
+        <AddTaskModal
+            isVisible={modalVisible}
+            onClose={() => setModalVisible(false)}
+            onCreate={handleCreateTask}
+        />
+        </View>
+    );
+};
+export default Home;
 
 const styles = StyleSheet.create({
     container: {
@@ -192,10 +214,8 @@ const styles = StyleSheet.create({
         // justifyContent: 'center',
         // alignItems: 'center',
         backgroundColor: '#fff',
-
     },
     headerContainer: {
-        
         borderWidth: 1,
         width: '100%',
         paddingTop: 45,
@@ -258,21 +278,21 @@ const styles = StyleSheet.create({
     clearButton: {
         marginLeft: 10,
     },
-        searchButton: {
-            borderWidth: 1,
-            width: 55,
-            height: 55,
-            backgroundColor: '#fff',
-            borderRadius: 12,
-            alignItems: 'center',
-            justifyContent: 'center',
-            elevation: 4,
-            shadowColor: '#000',
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.1,
-            shadowRadius: 3,
-        },
-    
+    searchButton: {
+        borderWidth: 1,
+        width: 55,
+        height: 55,
+        backgroundColor: '#fff',
+        borderRadius: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+    },
+
     title: {
         fontSize: 24,
         marginBottom: 30,
@@ -301,10 +321,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         elevation: 6,
-
     },
     taskCard: {
-        flexDirection:'row',
+        flexDirection: 'row',
         borderWidth: 1,
         borderColor: '#ccc',
         borderRadius: 10,
@@ -316,7 +335,7 @@ const styles = StyleSheet.create({
         // shadowOpacity: 0.1,
         // shadowRadius: 15,
         // elevation: 3, // for Android shadow
-},
+    },
     taskItem: {
         fontSize: 16,
         // paddingVertical: 4,
@@ -325,5 +344,4 @@ const styles = StyleSheet.create({
         width: '100%',
         // borderWidth: 1,
     },
-
-})
+});
