6571818081287d2a535d86f9468b7f73d328e740
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-24 22:49:24 +0530

    feat: add user object

--- Src/Context/AppwriteContext.tsx
+++ Src/Context/AppwriteContext.tsx
@@ -1,35 +1,45 @@
 import { createContext, useContext, useState, useEffect } from "react"
 import { getCurrentUser } from "../Service/Service";
+import { Models } from "appwrite"; // for typing the User object
+
 
 interface AuthContextType {
     isLoggedIn : boolean;
     setIsLoggedIn : (status:boolean) => void;
+    user: Models.User | null; // store actual user object
+    setUser: (user: Models.User<any> | null) => void;
 
 }
 
 export const AuthContext = createContext<AuthContextType>({
     isLoggedIn: false,
     setIsLoggedIn: ()=> {},
+    user: null,
+    setUser: ()=> {},
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [user, setUser] = useState<Models.User | null>(null);
   // Check user session on app start
   useEffect(() => {
     getCurrentUser().then((user) => {
       if (user) {
+        setUser(user);
         setIsLoggedIn(true);
       } else {
+        setUser(null);
         setIsLoggedIn(false);
       }
     });
   }, []);
 
   return(
-    <AuthContext.Provider value={{isLoggedIn,setIsLoggedIn}}>
+    <AuthContext.Provider value={{isLoggedIn,setIsLoggedIn,user,setUser}}>
         {children}
     </AuthContext.Provider>
   );
 
 }
+
+export const useAuth = () => useContext(AuthContext);
