48588b830d625161e864b04cd2582d87f5592d10
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-08-30 22:39:23 +0530

    feat: update prop and add isDeleted

--- Src/Types/Task.ts
+++ Src/Types/Task.ts
@@ -6,10 +6,13 @@ export type Task = {
     timestamp:string,        // or string if you use UUID
     // category?: [string];
     description?: string;
-    dueDateTime?: string; // ISO date string
+    dueDateTime?: string | null; // ISO date string
     completed?: boolean;
     isSynced?: boolean; // used to check if the task is synced with the appwrite or not
-    userId: string; 
+    userId: string;
+    photoId?: string | null; // stores the Appwrite File ID
+    photoPath?: string | null; // stores the local device path to the image
+    isDeleted?: boolean;
     success?: (res: any) => void;
     error?: (err: any) => void;
 };
